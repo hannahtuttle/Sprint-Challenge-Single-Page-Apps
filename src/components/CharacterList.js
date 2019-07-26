2c8a13ee7c32b668c04d25623e85dbe08be075f4
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-07-26 08:22:27 -0700

    read through direction, added semantic ui and styled components started working character list

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([])
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
+    axios.get()
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, [])
 
