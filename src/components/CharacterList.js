00ef36a61abb5d89ca95fd464c0e4419f6800135
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-07-26 08:59:19 -0700

    getting correct info from api, used semantic-ui card, fixed errors, starting on styles

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,19 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 
+
+import CharacterCard from './CharacterCard'
+
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [character, setCharacter] = useState([])
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
-    axios.get()
+    axios.get(`https://rickandmortyapi.com/api/character/`)
+    .then(response => {
+      console.log(response.data.results)
+      setCharacter(response.data.results)
+    }).catch(error => {
+      console.log('Not getting info from api', error)
+  })
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, [])
 
-  return <section className='character-list grid-view'>
-
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+  return (<section className='character-list grid-view'>
+    <div>{character.map(person => {
+        return <CharacterCard key={person.id} people={person}/>
+      })}</div>
+      {/* <h2>TODO: `array.map()` over your state here!</h2> */}
+    </section>)
 
 }
